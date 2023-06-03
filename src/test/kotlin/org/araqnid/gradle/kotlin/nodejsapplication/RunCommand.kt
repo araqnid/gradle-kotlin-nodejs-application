b6eb6d2adf55c00b354b64c0f4450ed168496f9f
Steve Haslam <araqnid@gmail.com>
2023-06-03 11:18:44 +0100

    Eliminate unnecessary Job variables
    No benefit waiting for stdin to close. Launching stderr and then
    immediately joining it can simply be written as withContext.

--- src/test/kotlin/org/araqnid/gradle/kotlin/nodejsapplication/RunCommand.kt
+++ src/test/kotlin/org/araqnid/gradle/kotlin/nodejsapplication/RunCommand.kt
@@ -1,41 +1,33 @@
 package org.araqnid.gradle.kotlin.nodejsapplication
 
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
+import kotlinx.coroutines.*
 import kotlinx.coroutines.future.await
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
 
-fun runCommand(vararg args: String): String {
-    return runBlocking {
-        @Suppress("BlockingMethodInNonBlockingContext")
-        val process = Runtime.getRuntime().exec(args)
+fun runCommand(vararg args: String): String = runBlocking {
+    @Suppress("BlockingMethodInNonBlockingContext")
+    val process = Runtime.getRuntime().exec(args)
 
-        val stdin = launch(Dispatchers.IO) {
-            process.outputStream.close()
-        }
-
-        val stdout = async(Dispatchers.IO) {
-            process.inputStream.reader().use { r ->
-                r.readText()
-            }
-        }
-
-        val stderr = launch(Dispatchers.IO) {
-            process.errorStream.bufferedReader().useLines { lines ->
-                for (line in lines) {
-                    println("node: stderr: $line")
-                }
-            }
-        }
-
-        stdin.join()
-        stderr.join()
-
-        process.onExit().await()
-        if (process.exitValue() != 0)
-            error("node process exited with ${process.exitValue()}")
-
-        stdout.await()
+    launch(Dispatchers.IO) {
+        process.outputStream.close()
     }
+
+    val stdout = async(Dispatchers.IO) {
+        process.inputStream.reader().use { r ->
+            r.readText()
+        }
+    }
+
+    withContext(Dispatchers.IO) {
+        process.errorStream.bufferedReader().useLines { lines ->
+            for (line in lines) {
+                println("node: stderr: $line")
+            }
+        }
+    }
+
+    process.onExit().await()
+    if (process.exitValue() != 0)
+        error("node process exited with ${process.exitValue()}")
+
+    stdout.await()
 }
